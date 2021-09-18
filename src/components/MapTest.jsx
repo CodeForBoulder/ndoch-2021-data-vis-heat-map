788ead8d2733527dafd0cf660c42238045e7cf30
jacobvenable <jacob.venable@gmail.com>
2021-09-18 13:11:23 -0600

    feat: add placeholder map

---
+++ src/components/MapTest.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
+
+const MapTest = () => {
+  console.log(
+    "process.env.REACT_APP_GOOGLE_MAPS_API_KEY: ",
+    process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  );
+  return (
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+        {/* Child components, such as markers, info windows, etc. */}
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default MapTest;
