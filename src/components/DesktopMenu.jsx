26fe63e23a8b6ddc5b194d9e5a94f3baedb1703a
jacobvenable <jacob.venable@gmail.com>
2021-09-18 18:12:14 -0600

    chore: remove unused useState from desktop menu

--- src/components/DesktopMenu.jsx
+++ src/components/DesktopMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
